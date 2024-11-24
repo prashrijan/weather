0edd3c7eed44e3413bd0d8958a40a7981b1e0b4f
Prashrijan <shresthaprashrijan@gmail.com>
2024-11-24 21:27:08 +1100

    ui readt

--- script.js
+++ script.js
@@ -1,2 +1,2 @@
-const API_KEY = "1476ae6554527c8bb8a179fe0c758dd9";
-const endPoint = "https://api.openweathermap.org/data/2.5/weather?q="; // &appid = {API_KEY}
+const WEATTHER_API_KEY = "1476ae6554527c8bb8a179fe0c758dd9";
+const weather_api = "https://api.openweathermap.org/data/2.5/weather?q="; // &appid = {API_KEY}
